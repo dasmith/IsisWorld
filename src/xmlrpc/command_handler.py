95e8f5c2d6bc3b17efd1cce4d1a2fb07bce3bf37
Bo Morgan <bo@mit.edu>
2010-09-01 10:56:51 -0400

    added queues to xmlrpc command_handler in order to make thread safe

--- src/xmlrpc/command_handler.py
+++ src/xmlrpc/command_handler.py
@@ -86,6 +86,13 @@ class IsisCommandHandler(object):
                 seconds = args['seconds']
             self.simulator.physicsManager.stepSimulation(seconds)
             
+            # Bo says: I remarked these lines out, so we need to find
+            # another way to block time before returning to the
+            # client.  The command queues mean we can't sleep because
+            # all of these functions are called from within a Panda3D
+            # Task now.  The xmlrpc thread shouldn't call -any-
+            # Panda3D functions because they are not thread safe.
+            #
             #time.sleep(seconds)
             ## dont accept new commands until this has stepped
             #while self.simulator.physicsManager.stepping:   self.closed = True

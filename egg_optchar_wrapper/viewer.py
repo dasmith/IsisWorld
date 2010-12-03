8b2768a1c0889176472eb3e618d6c945c40ad68b
Rahul Rajagopalan <rahulrajago@gmail.com>
2010-12-02 23:26:49 -0500

    Got the program to display a the panda model, trying to generalize it now

--- egg_optchar_wrapper/viewer.py
+++ egg_optchar_wrapper/viewer.py
@@ -1,8 +1,47 @@
+from math import pi, sin, cos
+
 import os
 import sys
 import direct.directbase.DirectStart
+from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
+from direct.actor.Actor import Actor
 
-if __name__ == '__main__':
+class ModelDisplayer(ShowBase):
+    
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        # Load the environment model
+        self.environ = self.loader.loadModel("models/environment")
+        # Reparent model to render
+        self.environ.reparentTo(self.render)
+        # Apply scale and position transforms
+        self.environ.setScale(0.25, 0.25, 0.25)
+        self.environ.setPos(-8, 42, 0)
+        
+        # Add the spinCameraTask
+        self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+
+        self.addAModel()
+        
+    def spinCameraTask(self, task):
+        angleDegrees = task.time * 6.0
+        angleRadians = angleDegrees * (pi / 180.0)
+        self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
+        self.camera.setHpr(angleDegrees, 0, 0)
+        return Task.cont
+
+    def addAModel(self):
+        self.pandaActor = Actor("models/panda-model")
+        self.pandaActor.setScale(0.005, 0.005, 0.005)
+        self.pandaActor.reparentTo(self.render)
+        #self.pandaActor.loop("walk") 
+
+md = ModelDisplayer()
+md.run()
+
+def start():
     print "sys.argv: ", sys.argv
     print "len(sys.argv) ", len(sys.argv)
     if len(sys.argv) != 2:
